9c419649654e9923ab5f911c5713d1abacc6e0af
DEBARD Cedric <cdebard@gmail.com>
2026-06-14 07:30:17 -0400

    tesk4

---
+++ EmotionDetection/emotion_detection.py
@@ -0,0 +1,25 @@
+import requests
+import json
+
+def emotion_detector(text_to_analyze):
+    #print(text_to_analyze)
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+
+    myobj = { "raw_document": { "text": text_to_analyze } }
+    # Create a dictionary with the text to be analyzed
+    header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    # Set the headers required for the API request
+    response = requests.post(url, json = myobj, headers=header)
+    # Send a POST request to the API with the text and headers
+    #print(response)
+    formatted_response = json.loads(response.text)
+    emotion = formatted_response['emotionPredictions'][0]['emotion']
+    result = emotion.copy()
+    hi_score = -1
+
+    for k, v in emotion.items():
+        if v > hi_score:
+            hi_score = v
+            result['dominant_emotion'] = k
+
+    return(result)
